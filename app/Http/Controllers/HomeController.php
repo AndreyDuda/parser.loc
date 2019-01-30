e7ccaf30491de2b670d3a702d1f1a74bf41793c1
AndreyDuda <andreyduda88@gmail.com>
2019-01-30 23:21:26 +0200

    test crawler

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\Parser\NewsParserService;
 use Illuminate\Http\Request;
 use Symfony\Component\DomCrawler\Crawler;
+use App\Service\Parser\ProductParserService;
 
 class HomeController extends Controller
 {
@@ -11,11 +13,17 @@ class HomeController extends Controller
     private $page;
     private $count;
 
-    public function __construct()
+    private $parser;
+
+    private $html;
+
+    public function __construct(NewsParserService $parser)
     {
         $this->link  = 'https://hi-news.ru/';
         $this->page  = 'page/';
         $this->count = 2;
+
+        $this->parser = $parser;
 
     }
 
@@ -24,64 +32,10 @@ class HomeController extends Controller
         $array = [];
 
         for ($i = 1; $i <= $this->count; $i++) {
-            $array[] = $this->getLinks($this->link.$this->page.$i);
+            $array[] = $this->parser->parse($this->link.$this->page.$i);
         }
 
         dd($array);
         return view('welcome');
     }
-
-    public function getLinks($link)
-    {
-
-
-        $array = [];
-
-
-        $html = file_get_contents($link);
-        $crawler = new Crawler(null, $link);
-        $crawler->addHtmlContent($html, 'UTF-8');
-
-        $title = $crawler->filter('div > h2')->text();
-
-
-        $links = $crawler->filter('#main > #content')->each(function (Crawler $node, $i) {
-            return $node->filter('h2 > a')->each(function (Crawler $node, $i) {
-                return $node->attr('href');
-            });
-        });
-        /*dd($links);*/
-        foreach ($links[0] as $link)
-        {
-
-          $array[] = $this->getContent($link);
-
-        }
-
-        return $array;
-
-    }
-
-    public function getContent($link)
-    {
-        $data = [];
-
-        $html = file_get_contents($link);
-        $crawler = new Crawler(null, $link);
-        $crawler->addHtmlContent($html, 'UTF-8');
-
-        $data['title'] = $crawler->filter('.single-title')->text();
-        $data['content'] = $crawler->filter('.text')->text();
-        $data['image'] = $crawler->filter('.text > p > img')->attr('src');
-
-        $data['image'] = $crawler->filter('.text img')->each( function (Crawler $node, $i) {
-            return 'http:'.$node->attr('src');
-        });
-
-        return $data;
-    }
-
-
-
-
 }
